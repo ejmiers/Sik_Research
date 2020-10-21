539c42172656b2c7f6ef86517c4788b2e02edce2
Eric Miers <eric.miers.15@cnu.edu>
2020-10-20 21:26:55 -0400

    Refctor, added threshold function

--- signal_processing/energy_detecting_filter.py
+++ signal_processing/energy_detecting_filter.py
@@ -10,51 +10,68 @@
 # October 17, 2020
 
 import numpy as np
+from scipy.stats import norm
 
-# Input and Output Files
+def calculateThreshold(noiseAmplitude, probabilityFA, n):
+    return noiseAmplitude**2 * (abs(norm.ppf(probabilityFA)) * (2*n)**0.5 + n)
+
+
+def filterData(rawData, blockSize, threshold):
+
+    blockNum = 0
+
+    # Implements the sliding window algorithm
+    while rawData.shape[0] > 0:
+
+        # Check to see if the next block is cut off by the end of the array
+        if rawData.shape[0] > blockSize:
+            blockEnd = blockSize
+        else:
+            blockEnd = rawData.shape[0]
+        
+        # Perform FFt on block, convert to squared magnitude
+        block = np.fft.fft(rawData[0:blockEnd])
+        blockMean = np.mean(np.square(np.absolute(block)))
+
+        # Add the samples in the block to the filtered sample array if the block's mean is greater or equal to the threshold
+        if blockMean >= threshold:
+            print("block {} above threshold".format(blockNum))
+            filteredData.extend(np.fft.ifft(block).astype('float32').tolist())
+
+        # Re-slice array by removing the current block
+        rawData = rawData[blockEnd:]
+        blockNum += 1
+    
+    return filteredData
+
+# Input File
 dataFile = "../../Lab PC/Data/Sik_Capture_Raw.data"
-newFile = "../../Lab PC/Data/Sik_Capture_Filtered2_1e-3Thresh.data"
+
 
 # Load the input file into numpy array and convert the array to real values 
-rawData = np.fromfile(dataFile, dtype=np.complex64)
+#rawData = np.fromfile(dataFile, dtype=np.complex64)
 
 # Use a standard list for filtered data
 # Python list allows for faster re-allocation than numpy array
 # Memory overhead is sacrificed for time
 filteredData = []
 
-# Define the threshold and sample block size.
-threshold = 1e-3
+# Define the https://github.com/ejmiers/Sik_Research.git://github.com/ejmiers/Sik_Research.gitreshold and sample block size.
+#threshold = 1e-3
 blockSize = 2048
+threshold = calculateThreshold(1, 0.10, blockSize)
 
-blockNum = 0
+# Filter the Dataset to only include samples above the threshold
+#filteredData = filterData(rawData, blockSize, threshold)
 
-# Implements the sliding window algorithm
-while rawData.shape[0] > 0:
-
-    # Check to see if the next block is cut off by the end of the array
-    if rawData.shape[0] > blockSize:
-        blockEnd = blockSize
-    else:
-        blockEnd = rawData.shape[0]
-    
-    # Perform FFt on block, convert to squared magnitude
-    block = np.fft.fft(rawData[0:blockEnd])
-    blockMean = np.mean(np.square(np.absolute(block)))
-
-    # Add the samples in the block to the filtered sample array if the block's mean is greater or equal to the threshold
-    if blockMean >= threshold:
-        print("block {} above threshold".format(blockNum))
-        filteredData.extend(np.fft.ifft(block).astype('float32').tolist())
-
-    # Re-slice array by removing the current block
-    rawData = rawData[blockEnd:]
-    blockNum += 1
+# Output File
+newFile = "../../Lab PC/Data/Sik_Capture_Filtered_{:4f}.data".format(threshold)
 
 # Write the filtered data to the output file
-np.array(filteredData).astype('float32').tofile(newFile)
+#np.array(filteredData).astype('float32').tofile(newFile)
 
 # Log the number of samples that met the threshold
 print("")
+print("Energy Level Threshold: {}".format(threshold))
 print("Final Number of samples: {}".format(len(filteredData)))
 print("")
