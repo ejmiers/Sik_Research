b8c733a307570a4c34d11eee0ee5a4c9757eaaa4
Eric <ejmiers@msn.com>
2020-12-04 17:26:27 -0500

    Refactored - Adjusted hyperparameters, implemented earlystopping, removed separation of train/test sets

--- ml/simulation_classification.py
+++ ml/simulation_classification.py
@@ -8,27 +8,23 @@ import numpy as np
 import os
 
 # Globals
-noiseAmp = 0.01
 PATH = "/media/ericmiers/Grad School Data/Research/Data/Simulated Signals/"
 DEVICES = ["dev_0", "dev_1", "dev_2", "dev_3"]
 
+noiseAmp = 0.01
+bestModelPath = PATH + 'models/best_model.h5'
+numEpochs = 130
 #=============================================================================================
 
-# Load Training Data
-X_train = np.load("{}{}/samples2_{}.npy".format(PATH, "train", noiseAmp))
-Y_train = np.load("{}{}/labels2_{}.npy".format(PATH, "train", noiseAmp))
-
-# Load Testing Data
-X_test = np.load("{}{}/samples2_{}.npy".format(PATH, "test", noiseAmp))
-Y_test = np.load("{}{}/labels2_{}.npy".format(PATH, "test", noiseAmp))
+# Load Data
+X = np.load("{}samples_{}.npy".format(PATH, noiseAmp))
+Y = np.load("{}labels_{}.npy".format(PATH, noiseAmp))
 
 # Make sure data is the correct type
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+X = X.astype('float32')
 
-# Shuffle Training and Testing Data
-X_train, Y_train = shuffle(X_train, Y_train)
-X_test, Y_test = shuffle(X_test, Y_test)
+# Shuffle Data
+X, Y = shuffle(X, Y)
 
 #=============================================================================================
 
@@ -37,36 +33,33 @@ numFolds = 10
 foldAccuracy = []
 foldLoss = []
 
-# inputs = np.concatenate((X_train, X_test))
-# targets = np.concatenate((Y_train, Y_test))
-
 kfold = KFold(n_splits=numFolds, shuffle=True)
 
 #=============================================================================================
 
 fold = 1
-for train, validate in kfold.split(X_train, Y_train):
+for train, validate in kfold.split(X, Y):
     # Build the DNN model
     RegularizedDense = partial(keras.layers.Dense,
-                            activation="relu"
-                            # kernel_initializer="he_normal", 
+                            activation="elu",
+                            kernel_initializer="he_normal", 
                             # kernel_regularizer=keras.regularizers.l2(0.01)
                             )
 
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=[2,128]),
-        # keras.layers.BatchNormalization(),
+        #keras.layers.BatchNormalization(),
         # keras.layers.Dropout(rate=0.2),
         RegularizedDense(300),
-        # keras.layers.BatchNormalization(),
+        #keras.layers.BatchNormalization(),
         # keras.layers.Dropout(rate=0.2),
-        RegularizedDense(100),
-        # keras.layers.BatchNormalization(),
+        RegularizedDense(300),
+        #keras.layers.BatchNormalization(),
         # keras.layers.Dropout(rate=0.2),
-        # RegularizedDense(100),
-        # keras.layers.BatchNormalization(),
+        RegularizedDense(300),
+        #keras.layers.BatchNormalization(),
         # keras.layers.Dropout(rate=0.2),
-        # RegularizedDense(100),
+        #RegularizedDense(100),
         # keras.layers.BatchNormalization(),
         # keras.layers.Dropout(rate=0.2),
         keras.layers.Dense(4, activation="softmax")
@@ -74,35 +67,29 @@ for train, validate in kfold.split(X_train, Y_train):
 
     model.compile(loss = "sparse_categorical_crossentropy",
                 optimizer = keras.optimizers.SGD(lr=0.01, momentum=0.9),
+                #optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999),
                 metrics=["accuracy"])
-
-    # if fold == 1:
-    #     print('\n=============================================================')
-    #     print(f'Initial Weights:\n')
-    #     print(model.get_weights())
 
 
     print('\n=============================================================')
     print(f'Training fold {fold}...\n')
 
-    # Train the model
-    history = model.fit(X_train[train], Y_train[train], epochs=100)
+    # Implement early stopping with checkpoints to curb overfitting
+    es = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=10)
+    mc = keras.callbacks.ModelCheckpoint(bestModelPath, monitor='val_acc', mode='max', verbose=1, save_best_only=True)
 
-    # Validate the model
-    scores = model.evaluate(X_train[validate], Y_train[validate])
+    # Train the model
+    history = model.fit(X[train], Y[train], validation_data=(X[validate], Y[validate]), epochs=numEpochs, callbacks=[es, mc])
+
+    # Validate the best model
+    bestModel = keras.models.load_model(bestModelPath)
+    scores = bestModel.evaluate(X[validate], Y[validate])
     foldLoss.append(scores[0])
     foldAccuracy.append(scores[1] * 100)
     
-    print(f"Fold Results: Loss={scores[0]}, Accuracy={scores[1]}")
+    print(f"Fold Results (Best Model): Loss={scores[0]}, Accuracy={scores[1]}")
 
     if fold == 10:
-
-        # print('\n=============================================================')
-        # print(f'Final Weights:\n')
-        # print(model.get_weights())
-
-        # Test model on novel signal
-        testScores = model.evaluate(X_test, Y_test)
 
         print('\n=====================Final Results=========================\n')
         for i in range(len(foldAccuracy)):
@@ -112,14 +99,12 @@ for train, validate in kfold.split(X_train, Y_train):
         print(f"\nTest on novel signal: Loss={testScores[0]}, Accuracy={testScores[1]}")
         print('\n===========================================================\n')    
 
-        model.save(f"{PATH}models/12_2_2020_10folds.h5")
+        bestModel.save(f"{PATH}models/12_4_2020_10folds.h5")
+
+        # Graph Epoch History of Final Fold
+        pd.DataFrame(history.history).plot(figsize=(8,5))
+        plt.grid(True)
+        plt.gca().set_ylim(0,1)
+        plt.show()
 
     fold += 1
-
-#=============================================================================================
-
-# # Graph results
-# pd.DataFrame(history.history).plot(figsize=(8,5))
-# plt.grid(True)
-# plt.gca().set_ylim(0,1)
-# plt.show()
